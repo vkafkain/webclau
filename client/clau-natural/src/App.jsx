5e62abc327251f610672f930f03ffc42ec767bc9
Víctor López <vlopezmoles@gmail.com>
2023-01-17 10:12:10 +0100

    init client

---
+++ client/clau-natural/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+  return (
+    <article>
+        <header>
+            <img alt="avatar" src="https://unavatar.io/victorlopez" />
+            <div>
+                <strong>Victor Lopez</strong>
+                <span>@victor</span>
+            </div>
+        </header>
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+    </article>
+  )
+}
+
