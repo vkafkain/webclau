c62960d1ccade1dd2a4cbe6e1804f3ab77d6c1bc
Víctor López <vlopezmoles@gmail.com>
2023-01-20 12:22:20 +0100

    creating login, register, index components

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,14 @@
 export function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  async function login(){
+    ev.preventDefault();
+    await fetch('localhost:3000/login'), {
+      method: POST,
+    }
+  }
   return (
-    <form className="login">
+    <form className="login" onSubmit>
         <h1>Login</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
